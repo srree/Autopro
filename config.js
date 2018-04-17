15ae18dfc2db067443009dc1b76fbeeb37652d37
Sridevi Reddy Kunam <sridevi@oti.com>
2018-04-17 17:49:49 +0530

    made changes

--- config.js
+++ config.js
@@ -1,59 +1,39 @@
+// An example configuration file
 exports.config = {
-    framework: 'jasmine',
+    // The address of a running selenium server.
     seleniumAddress: 'http://localhost:4444/wd/hub',
-       ignoreUncaughtExceptions: true,
-       capabilities: {
-           'browserName' : 'chrome'
-       },
-    //specs: ['Users/vishvaprasad/eclipse-workspace/ILBspec.js']
-   //specs: ['ilbLogin.js'],
-   //    
-   //         plugins: [{
-   //    package: 'protractor-testability-plugin'
-   //    }],
-//   
-   // specs:['C:/Users/sridevi/Protractor/sample.js'],
-    //specs:['C:/Users/sridevi/Protractor/menu.js'],
-    specs:['C:/Users/sridevi/Protractor/homepage.js'],
-    
-    //    suites: {
-
-    // //   exploreLanguage: '/Users/vishvaprasad/eclipse-workspace/ILB/exploreLanguage.js',
-    // //   //languagedonations: '/Users/vishvaprasad/eclipse-workspace/ILB/languagedonations.js'
-    // //    projectdonations: '/Users/vishvaprasad/eclipse-workspace/ILB/projectdonations.js'
-    // },
-       //specs: ['ilbLogin.js'],
-    
-   
-    // params: {
-    //   login: {
-    //     email: 'oliveqa345@mailinator.com',
-    //     password: '123456'
-    //   },
-    //     donations: {
-    //         monthly: 'yes',
-    //         receiveEmailPrayerUpdates: 'yes'
-    //     }
-    // },
-   //    capabilities: {
-   //    browserName: 'firefox'
-   //  }
-   //    multiCapabilities: [{
-   //    browserName: 'firefox'
-   //  }, {
-   //    browserName: 'chrome'
-   //  }]
-   
-   onPrepare: function(){
-       browser.manage().window().maximize();
-       browser.manage().timeouts().implicitlyWait(900000);
-   },
-
-
-       jasmineNodeOpts:{
-           showColors: true,
-           defaultTimeoutInterval:80000000,
-           
-       },
+  
+    // Capabilities to be passed to the webdriver instance.
+   /* capabilities: {
+      browserName: 'chrome'
+    },*/
+    getPageTimeout: 5000000,
+    onPrepare: function() {
+     
+      browser.driver.manage().window().maximize();
+  },
+    Capabilities: [{
+      
        
-   }
+      browserName: 'chrome'
+      
+      }],
+      
+    
+
+      spec:['C:/Users/sridevi/Automation/ILB/sample.js'],
+    // Spec patterns are relative to the configuration file location passed
+    // to protractor (in this example conf.js).
+    // They may include glob patterns.
+   
+
+    
+   spec:['C:/Users/sridevi/Automation/ILB/AboutFromMenu.js'],
+  
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+      showColors: true, // Use colors in the command line report.
+     
+    defaultTimeoutInterval : 50000000
+    }
+  };
